cbbd6561ae7c3a090a0fb66ce68da2ee6303ee65
eukk <nguyenkhiem1812.206@gmail.com>
2026-09-19 02:27:55 +0700

    add visualize and sudoku problem

--- n_queens.py
+++ n_queens.py
@@ -1,25 +1,27 @@
 from pysat.solvers import Solver
 import pairwise_amo, bitwise_amo, commander_amo, sequential_amo, product_amo
 
-def solve_n_queens(n: int, type_amo=pairwise_amo, name_amo='Pairwise') -> None:
+# Solve n-queens problem
+def solve_n_queens(board: list, n: int, type_amo=pairwise_amo, name_amo='Pairwise') -> None:
     # Initialization
-    board = [[i*n + j + 1 for j in range(n)] for i in range(n)]
-    literals = [e for row in board for e in row]
-    current_id = n * n
     clauses = []
+    current_id = n * n
+    def get_id(x: int, y: int) -> int:
+        return x*n + y + 1
+    literals = set(get_id(r, c) for r in range(n) for c in range(n))
 
     # Row constraint
     for r in range(n):
-        row_literals = board[r]
+        row_literals = [get_id(r, c) for c in range(n)]
         clauses.append(row_literals) # ALO
-        c, au, current_id = type_amo.encode(row_literals, current_id)
-        clauses.extend(c)
+        cl, au, current_id = type_amo.encode(row_literals, current_id)
+        clauses.extend(cl)
 
     # Column constraint
     for c in range(n):
-        column_literals = [board[r][c] for r in range(n)]
-        c_cls, au, current_id = type_amo.encode(column_literals, current_id)
-        clauses.extend(c_cls)
+        column_literals = [get_id(r, c) for r in range(n)]
+        cl, au, current_id = type_amo.encode(column_literals, current_id)
+        clauses.extend(cl)
 
     # Diag constraint
     diag_size = 2*n - 1
@@ -28,17 +30,24 @@ def solve_n_queens(n: int, type_amo=pairwise_amo, name_amo='Pairwise') -> None:
     for r in range(n):
         for c in range(n):
             m = r-c+(n-1)
-            main_diags[m].append(board[r][c])
+            main_diags[m].append(get_id(r, c))
             s = r+c
-            sub_diags[s].append(board[r][c])
+            sub_diags[s].append(get_id(r, c))
     for e in main_diags:
         if len(e) > 1:
-            c, au, current_id = type_amo.encode(e, current_id)
-            clauses.extend(c)
+            cl, au, current_id = type_amo.encode(e, current_id)
+            clauses.extend(cl)
     for e in sub_diags:
         if len(e) > 1:
-            c, au, current_id = type_amo.encode(e, current_id)
-            clauses.extend(c)
+            cl, au, current_id = type_amo.encode(e, current_id)
+            clauses.extend(cl)
+
+    # For some reserved cells
+    for r in range(n):
+        for c in range(n):
+            value = board[r][c]
+            if value != 0:
+                clauses.append([get_id(r, c)])
 
     # Solve
     print(f'Type of AMO: {name_amo}')
@@ -47,10 +56,13 @@ def solve_n_queens(n: int, type_amo=pairwise_amo, name_amo='Pairwise') -> None:
         for c in clauses:
             solver.add_clause(c)
         if solver.solve():
-            model = solver.get_model()
-            # print(f'Solution with auxiliary literals: {model}')
+            model = set(solver.get_model())
             result = [(x if x in model else -x) for x in literals]
             print(f'Solution with main literals: {result}')
+            # Board Visualization
+            for r in range(n):
+                for c in range(n):
+                    print(1 if get_id(r, c) in model else 0, end = ' ' if c < n-1 else '\n')
         else:
             print('No solution found!')
     except Exception as e:
@@ -61,8 +73,9 @@ def solve_n_queens(n: int, type_amo=pairwise_amo, name_amo='Pairwise') -> None:
     print()
 
 if __name__ == '__main__':
-    solve_n_queens(4)
-    solve_n_queens(4, bitwise_amo, name_amo='Bitwise')
-    solve_n_queens(4, commander_amo, name_amo='Commander')
-    solve_n_queens(4, sequential_amo, name_amo='Sequential')
-    solve_n_queens(4, product_amo, name_amo='Commander')
+    n = 8
+    board = [[0 for j in range(n)] for i in range(n)]
+    board[0][0] = 1
+    board[2][3] = 1
+    print(board)
+    solve_n_queens(board, n, sequential_amo, name_amo='Sequential')
---
+++ n_square_sudoku.py
@@ -0,0 +1,93 @@
+from pysat.solvers import Solver
+import pairwise_amo, bitwise_amo, commander_amo, sequential_amo, product_amo
+
+# Solve sudoku board n² x n²
+def solve_n_sudoku(board: list, n: int, type_amo=pairwise_amo, name_amo='Pairwise') -> None:
+    # Initialization
+    size = n*n
+    clauses = []
+    current_id = size*size*size
+    def get_id(x: int, y: int, value: int) -> int:
+        return x*size*size + y*size + value
+    literals = set(get_id(i, j, value) for i in range(size) for j in range(size) for value in range(1, size+1))
+    def add_exactly_one(literals_: list) -> None:
+        nonlocal current_id
+        clauses.append(literals_) # ALO
+        cl, au, current_id = type_amo.encode(literals_, current_id)
+        clauses.extend(cl)
+
+    # Cell constraint
+    for r in range(size):
+        for c in range(size):
+            add_exactly_one([get_id(r, c, value) for value in range(1, size+1)])
+
+    # Row constraint
+    for c in range(size):
+        for value in range(1, size+1):
+            add_exactly_one([get_id(r, c, value) for r in range(size)])
+
+    # Column constraint
+    for r in range(size):
+        for value in range(1, size+1):
+            add_exactly_one([get_id(r, c, value) for c in range(size)])
+
+    # Sub-board constraint (n x n)
+    for sr in range(n):
+        for sc in range(n):
+            for value in range(1, size+1):
+                lits = []
+                for offset_r in range(n):
+                    for offset_c in range(n):
+                        r = sr*n + offset_r
+                        c = sc*n + offset_c
+                        lits.append(get_id(r, c, value))
+                add_exactly_one(lits)
+
+    # For some pre-numbered cells
+    for r in range(size):
+        for c in range(size):
+            value = board[r][c]
+            if value != 0:
+                clauses.append([get_id(r, c, value)])
+
+    # Solve
+    print(f'Type of AMO: {name_amo}')
+    solver = Solver(name='glucose4')
+    try:
+        for c in clauses:
+            solver.add_clause(c)
+        if solver.solve():
+            model = set(solver.get_model())
+            result = [(x if x in model else -x) for x in literals]
+            print(f'Solution with main literals: {result}')
+            # Board Visualization
+            for r in range(size):
+                for c in range(size):
+                    for value in range(1, size+1):
+                        if get_id(r, c, value) in model:
+                            print(value, end = ' ' if c < size-1 else '\n')
+                            break
+        else:
+            print('No solution found!')
+    except Exception as e:
+        print(f'Error: {e}')
+    finally:
+        if solver is not None:
+            solver.delete()
+    print()
+
+
+if __name__ == "__main__":
+    n = 3
+    board = [
+        [0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [2, 0, 0, 0, 3, 0, 4, 0, 5],
+        [0, 6, 7, 0, 8, 0, 0, 1, 0],
+        [1, 0, 4, 3, 0, 0, 0, 9, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 5, 0, 0, 0, 2, 8, 0, 6],
+        [0, 7, 0, 0, 4, 0, 3, 2, 0],
+        [9, 0, 8, 0, 5, 0, 0, 0, 1],
+        [0, 0, 0, 7, 0, 0, 0, 0, 0]
+    ]
+    solve_n_sudoku(board, n, bitwise_amo, name_amo='Bitwise')
